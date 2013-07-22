e7e26f214d9795e6d423093e964c49c935e26610
maxnovak <novak.max@gmail.com>
2013-07-22 17:15:28 -0400

    Now looks through files from the top and from the files will display all of them and make one of the folders the new path

--- generic.py
+++ generic.py
@@ -1,16 +1,29 @@
 import os, time
 top = "/"
 i = 1
-for root, dirs, files in os.walk(top, topdown=True):
-    print root #the path im starting at
+#for root, dirs, files in os.walk(top, topdown=True):
+    #print root #the path im starting at
     #print "--------------"
     #print dirs #should randomly choose which to go into
     #print "*****************"
     #print files #Don't care about files right now
+    #i += 1
+    #if i > 10:
+         #break
+
+#will make recursive just doing forloop for basic setup
+for i in range(10):
+    files = os.listdir(top)
+    for f in files:
+        path = os.path.join(top,f)
+        if os.path.isdir(path):
+            print path 
+            top = path
+            #recursive call here
+        else:
+            print path
+    
     time.sleep(0.05)
-    i += 1
-    if i > 10:
-        break
 
 
 
