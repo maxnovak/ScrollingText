c14e3e94110493c11946977f04929d1aac4bd054
maxnovak <novak.max@gmail.com>
2013-07-22 17:47:26 -0400

    made a function to recursively look through folders and display files within them.  Currently a permissions issue has been noted.

---
+++ randomwalk.py
@@ -0,0 +1,24 @@
+import os 
+import random
+import time
+
+def random_walk(top = "/"):
+    """recursively walks through folders and displays files, psuedo randomly jumps out of files and back up the path"""
+
+    files = os.listdir(top)
+    for f in files:
+        path = os.path.join(top,f)
+        if os.path.isdir(path):
+            print path
+            random_walk(path)
+        else:
+            print path
+        if len(top) >10:
+            break
+        if random.random() < 0.2:
+            break
+        time.sleep(0.05)
+
+if __name__ == "__main__":
+    random_walk()
+
