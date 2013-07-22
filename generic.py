1e06c452e03bd7a04bedae83dd9d53afae9a2c1a
maxnovak <novak.max@gmail.com>
2013-07-22 16:37:54 -0400

    Actually commited just one step in and displaying os.walk options.  Now correctly walks through directories, for ten steps.

--- generic.py
+++ generic.py
@@ -3,13 +3,13 @@ top = "/"
 i = 1
 for root, dirs, files in os.walk(top, topdown=True):
     print root #the path im starting at
-    print "--------------"
-    print dirs #should randomly choose which to go into
-    print "*****************"
-    print files #Don't care about files right now
+    #print "--------------"
+    #print dirs #should randomly choose which to go into
+    #print "*****************"
+    #print files #Don't care about files right now
     time.sleep(0.05)
     i += 1
-    if i > 1:
+    if i > 10:
         break
 
 
